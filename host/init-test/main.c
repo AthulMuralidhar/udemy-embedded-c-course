c0705aa0f3a10c9cc8d5d453f91c0f2e9bf5544e
Athul Muralidhar <athul.m68@gmail.com>
2024-05-11 17:58:37 +0200

    started with host proaject!! mr udupi mahadeva shit teach

---
+++ host/init-test/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: May 11, 2024
+ *      Author: athul-muralidhar
+ */
+
+
+#include<stdio.h>
+
+int main() {
+
+	printf("whathca whtcah thasd  \n");
+
+	printf("press enter iof u dare motha fucka! \n");
+
+	getchar();
+
+	return 0;
+}
