8ec3a14bc108cf9cf845eda880f433c629b07843
Athul Muralidhar <athul.muralidhar@print.com>
2024-05-14 23:32:18 +0200

    added sizeof c

---
+++ c-examples/sizeof.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+
+    printf("sizeof char: %ld\n",sizeof(char));
+    printf("sizeof int: %ld\n",sizeof(int));
+    printf("sizeof long: %ld\n",sizeof(long));
+    // printf("sizeof char: %ld\n",sizeof(char));
+    // printf("sizeof char: %ld\n",sizeof(char));
+
+    return 0;
+}
