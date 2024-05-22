12c637dfac97ea93a4630489d99f082df07aa8d3
Athul Muralidhar <athul.m68@gmail.com>
2024-05-22 21:39:47 +0200

    made the damn thing work!

--- target/helloworld/Src/main.c
+++ target/helloworld/Src/main.c
@@ -24,6 +24,24 @@
 
 #include <stdio.h>
 
+/*
+ * Problems:
+ * - this version of stm32ide does now come with proper support for gdb - the only thing to do then is use a local toolchain
+ * - this can be done by going to window > preferences - a window appears
+ * - in the search bar type tool chain manageer - you should see a side bar menu under stm32cube
+ * - click on add local
+ * Assuming you already have arm-none-eabi installed, open a terminal and run which arm-none-eabi-gcc to get its location. Here it is under /usr/bin.
+ * On the Toolchain Manager, click “Add Local…” button on the right, give it some name, leave the default arm-none-eabi- prefix as it is,
+ * then fill the Location with the location of your arm-none-eabi-* (possibly /usr/bin/). Click on Finish.
+ * Back on the Toolchain Manager, select the toolchain you’ve just added and click the “Set Default” button on the right.
+ *
+ * GOTCHA:
+ * - this might also be that the arm-none-eabi- expects a gdb - you probably have gdb-multiarch installed
+ * - now all is needed is to link it like so:
+ * `ln -s /usr/bin/gdb-multiarch /usr/bin/arm-none-eabi-gdb`
+ *
+ * */
+
 int main(void)
 {
 	printf("how\n");
